d9452dc9713d35f03a715734db8255b81f8631c9
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-11-11 15:50:37 +0100

    NutFarm2_5kyu - index out of bound no longer apppears, but does not pass tests on codewars.

--- src/main/codewars5kyu/NutFarm2_5kyu.java
+++ src/main/codewars5kyu/NutFarm2_5kyu.java
@@ -7,44 +7,50 @@ import java.util.List;
 class NutFarm2_5kyu {
 
     public static int[] shakeTree(final char[][] tree) {
-        int[] nutsPos = getNutsPos(tree);
-
+        List<int[]> nutIndexes = getNutsPos(tree);
         List<Integer> landingIndexes = new ArrayList<>();
-        for (int nut : nutsPos) {
-            int landingPos = fallNut(nut, tree);
+
+        for (int[] nut : nutIndexes) {
+            int landingPos = fallNut(nut[0],
+                    nut[1],
+                    tree);
             landingIndexes.add(landingPos);
         }
 
         return landingIndexes.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private static int fallNut(int nut, char[][] tree) {
-        int cPos = nut;
-        for (int cLevel = 0; cLevel < tree.length; cLevel++) {
+    private static int fallNut(int level, int nut, char[][] tree) {
+        int cPos = Math.max(0, Math.min(nut, tree[level].length - 1));
+
+        for (int cLevel = level; cLevel < tree.length; cLevel++) {
+            int width = tree[cLevel].length;
+            if (cPos < 0) return 0;
+            if (cPos >= width) return width - 1;
+
             char cChar = tree[cLevel][cPos];
-            if (cChar == '\\') {
-                cPos++;
-            } else if (cChar == '/') {
-                cPos--;
-            }
+
+            if (cChar == '\\') cPos++;
+            else if (cChar == '/') cPos--;
 
             if (cPos < 0) return 0;
-            if (cPos >= tree[0].length) return tree[0].length - 1;
+            if (cPos >= width) return width - 1;
         }
+
         return cPos;
     }
 
-    private static int[] getNutsPos(char[][] tree) {
-        List<Integer> nutsIndexes = new ArrayList<>();
+    private static List<int[]> getNutsPos(char[][] tree) {
+        List<int[]> nuts = new ArrayList<>();
 
-        for (int i = 0; i < tree[0].length; i++) {
-            char c = tree[0][i];
-            if ('o' == c) {
-                nutsIndexes.add(i);
+        for (int i = 0; i < tree.length; i++) {
+            for (int j = 0; j < tree[i].length; j++) {
+                if (tree[i][j] == 'o') {
+                    nuts.add(new int[]{i, j});
+                }
             }
         }
-
-        return nutsIndexes.stream().mapToInt(Integer::intValue).toArray();
+        return nuts;
     }
 
     public static void main(String[] args) {
